1ffc3f93bf5912e74eece85f17314cf0372c9369
Вова <gorkov2004@vk.com>
2026-02-22 14:05:38 +0300

    implement routers layer

--- internal/routers/router.go
+++ internal/routers/router.go
@@ -1,1 +1,32 @@
 package routers
+
+import (
+	"UrlShortener/internal/handlers"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"net/http"
+)
+
+// NewRouter create router with configured routes
+func NewRouter(handler *handlers.URLHandler) http.Handler {
+	r := chi.NewRouter()
+
+	//Logger middleware
+	r.Use(middleware.Logger)
+
+	// Middleware for recover from panic
+	r.Use(middleware.Recoverer)
+
+	// POST /shorten create short code by original url
+	r.Post("/shorten", handler.CreateURL)
+
+	// GET /{code} get original url by short code
+	r.Get("/{code}", handler.GetURL)
+
+	// GET /health health check
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Service health is OK"))
+	})
+
+	return r
+}
